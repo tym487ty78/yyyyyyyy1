802a5cb279ed6706bf36df68edfb4d1465193a9d
k V H v J T S F c u f X I s K Y D y q E p m d <candyhubcdev@gmail.com>
2024-12-23 00:25:44 +0100

    Create NextStep.lua

---
+++ NextStep.lua
@@ -0,0 +1,13 @@
+if game.PlaceId == 5708035517 or game.PlaceId == 6063653725 then
+	loadstring(game:HttpGet('https://pastebin.com/raw/pa40iK5b'))()
+elseif game.PlaceId == 14157644155 then
+	loadstring(game:HttpGet('https://pastebin.com/raw/wqFAwu31'))()
+elseif game.PlaceId == 11276071411 then
+	loadstring(game:HttpGet('https://pastebin.com/raw/yeqXz5vu'))()
+elseif game.PlaceId == 10260193230 then
+	loadstring(game:HttpGet('https://pastebin.com/raw/3ywWwACJ'))()
+elseif game.PlaceId == 116605585218149 then
+	loadstring(game:HttpGet('https://pastebin.com/raw/J2sY7DjT'))()
+else
+	print('game not supported')
+end
