71b404d4c542e54e8f373dca943fd9fbc6161734
tym487ty78 <xvateq@gmail.com>
2024-12-26 16:37:32 +0100

    Update NextStep.lua

--- NextStep.lua
+++ NextStep.lua
@@ -5,7 +5,7 @@ elseif game.PlaceId == 14157644155 then -- 0
 elseif game.PlaceId == 11276071411 then -- be npc or die
 	loadstring(game:HttpGet('https://pastebin.com/raw/yeqXz5vu'))()
 elseif game.PlaceId == 10260193230 then -- meme sea
-	loadstring(game:HttpGet('https://pastebin.com/raw/3ywWwACJ'))()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/tym487ty78/yyyyyyyy1/refs/heads/main/MemeSea.lua'))()
 elseif game.PlaceId == 116605585218149 then -- go fishing
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/tym487ty78/yyyyyyyy1/refs/heads/main/gofishing.lua'))()
 else
