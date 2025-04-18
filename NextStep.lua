157e57929abc2e09a767d1cbd82d712a793b8618
tym487ty78 <xvateq@gmail.com>
2025-04-18 15:02:14 +0200

    Update NextStep.lua

--- NextStep.lua
+++ NextStep.lua
@@ -1,9 +1,9 @@
 if game.PlaceId == 5708035517 or game.PlaceId == 6063653725 then -- mega hide and seek
-	loadstring(game:HttpGet('https://pastebin.com/raw/pa40iK5b'))()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/tym487ty78/yyyyyyyy1/refs/heads/main/megahideandseek.lua'))()
 elseif game.PlaceId == 14157644155 then -- 0
 	loadstring(game:HttpGet('https://pastebin.com/raw/wqFAwu31'))()
 elseif game.PlaceId == 11276071411 then -- be npc or die
-	loadstring(game:HttpGet('https://pastebin.com/raw/yeqXz5vu'))()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/tym487ty78/yyyyyyyy1/refs/heads/main/npcordie.lua'))()
 elseif game.PlaceId == 10260193230 then -- meme sea
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/tym487ty78/yyyyyyyy1/refs/heads/main/MemeSea.lua'))()
 elseif game.PlaceId == 116605585218149 then -- go fishing
@@ -11,9 +11,10 @@ elseif game.PlaceId == 116605585218149 then -- go fishing
 	print('script bannable W.I.P')
 elseif game.PlaceId == 10789933399 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/tym487ty78/yyyyyyyy1/refs/heads/main/geometryjump.lua'))()
-
 elseif game.PlaceId == 126884695634066 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/tym487ty78/yyyyyyyy1/refs/heads/main/growagarden.lua'))()
+elseif game.PlaceId == 301549746 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/tym487ty78/yyyyyyyy1/refs/heads/main/cbr.lua'))()
 else
 	print('game not supported')
 end
