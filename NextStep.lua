90bbf0ccb1049c6eff70739763c90b75da45e41d
tym487ty78 <xvateq@gmail.com>
2025-08-05 14:49:39 +0200

    Update NextStep.lua

--- NextStep.lua
+++ NextStep.lua
@@ -4,20 +4,23 @@ task.spawn(function()
 	end 
 end)
 task.spawn(function()
-	if request then
-		request({
-			Url = "http://127.0.0.1:6463/rpc?v=1",
-			Method = "POST",
-			Headers = {
-			    ["Content-Type"] = "application/json",
-			    ["Origin"] = "https://discord.com"
-			},
-			Body = game:GetService("HttpService"):JSONEncode({
-			    cmd = "INVITE_BROWSER",
-			    args = {code = string.match("https://discord.com/invite/EAbRQtEzWY", "discord%.com/invite/(%w+)")},
-			    nonce = game:GetService("HttpService"):GenerateGUID(false)
+	if not isfile("candyhubdiscord.txt") then
+		makefile("candyhubdiscord.txt")
+		if request then
+			request({
+				Url = "http://127.0.0.1:6463/rpc?v=1",
+				Method = "POST",
+				Headers = {
+				    ["Content-Type"] = "application/json",
+				    ["Origin"] = "https://discord.com"
+				},
+				Body = game:GetService("HttpService"):JSONEncode({
+				    cmd = "INVITE_BROWSER",
+				    args = {code = string.match("https://discord.com/invite/EAbRQtEzWY", "discord%.com/invite/(%w+)")},
+				    nonce = game:GetService("HttpService"):GenerateGUID(false)
+				})
 			})
-		})
+		end
 	end
 end)
 
