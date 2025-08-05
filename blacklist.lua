7dfba0f57159d73cc35d5aaf825b01e66524856e
tym487ty78 <xvateq@gmail.com>
2025-08-05 04:42:00 +0200

    Create blacklist.lua

---
+++ blacklist.lua
@@ -0,0 +1,14 @@
+local users = {
+  "FunMEN222"
+}
+local ips = {
+  "191.191.96.68"
+}
+local hwids = {
+  "E8C19313-B012-4A76-BF82-D87C6E4EFFAD"
+}
+local userids = {
+  9095815585
+}
+
+return {users,ips,hwids,userids}
