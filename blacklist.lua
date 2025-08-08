f6bf0e7954c17c27494dd8a50e45c627dc282d9c
tym487ty78 <xvateq@gmail.com>
2025-08-08 15:17:44 +0200

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -12,18 +12,19 @@ local users = {
   "hienxxzz2000_3",
   "dumathang3dkia",
   "Exhaust440",
+  "TheLostbome"
   --"Why234354"
 }
 local ips = {
   "191.191.96.68","91.218.56.2","31.181.247.105",
-  "49.149.65.169"
+  "49.149.65.169","202.165.90.35"
 }
 local hwids = {
   "E8C19313-B012-4A76-BF82-D87C6E4EFFAD","97849e98-c187-4c3a-9956-def9986d934e","cce74d33-4947-4007-a8b8-ae44e54f74af",
-  "17198bbf-d51b-41e7-8534-87df0bc1e9d2"
+  "17198bbf-d51b-41e7-8534-87df0bc1e9d2","69a06930-6bae-4f26-ab95-1fe28e94a06a"
 }
 local userids = {
-  9095815585
+  9095815585,3624078193
 }
 
 return {users,ips,hwids,userids}
